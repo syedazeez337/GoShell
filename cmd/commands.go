1ec44ff4cfc09fe9513d08866ebec148263465ca
Syed Azeez <syedazeez337@gmail.com>
2025-01-03 10:54:34 +0530

    Writing commands

--- cmd/commands.go
+++ cmd/commands.go
@@ -22,3 +22,40 @@ func ExecuteCommand(args []string) {
 		fmt.Println("Error:", err)
 	}
 }
+
+// handle buildin commands
+func HandleBuildInCommands(args []string) bool {
+	if len(args) == 0 {
+		return false
+	}
+
+	switch args[0] {
+	case "exit":
+		fmt.Println("Goodbye!")
+		os.Exit(0)
+	case "cd":
+		if len(args) < 2 {
+			fmt.Println("cd: missing argument")
+			return true
+		}
+		err := os.Chdir(args[1])
+		if err != nil {
+			fmt.Println("cd error:", err)
+		}
+		return true
+	case "pwd":
+		if len(args) < 2 {
+			fmt.Println("pwd: missing arguement")
+			return true
+		}
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println("Error:", err)
+			return true
+		}
+		fmt.Println(wd)
+		return true
+	}
+
+	return false
+}
